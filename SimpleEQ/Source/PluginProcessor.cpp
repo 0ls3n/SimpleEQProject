25206f91b2f38e459e128ea26749b7262e6de25f
Rasmus Olsen <rasmus782@gmail.com>
2023-10-14 22:38:47 +0200

    Created the parameters for the plugin
    Added all the parameters used by the EQ:
    LowCut, and slope
    HighCut and slope
    Peak
    Peak Gain
    Peak Quality

--- SimpleEQ/Source/PluginProcessor.cpp
+++ SimpleEQ/Source/PluginProcessor.cpp
@@ -166,7 +166,8 @@ bool SimpleEQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SimpleEQAudioProcessor::createEditor()
 {
-    return new SimpleEQAudioProcessorEditor (*this);
+    //return new SimpleEQAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -181,6 +182,34 @@ void SimpleEQAudioProcessor::setStateInformation (const void* data, int sizeInBy
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout SimpleEQAudioProcessor::createParameterLayout()
+{
+   juce::AudioProcessorValueTreeState::ParameterLayout layout; // Creating an instance which contains all the parameters used
+
+   // Adding the high- and lowcut parameters here
+   layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq", "LowCut Freq", juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f), 20.f)); // Added the lowcut frequency parameter inside the value tree
+   layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq", "HighCut Freq", juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f), 20000.f)); // Added the highcut frequency parameter inside the value tree
+
+   // Adding the peak freq, gain and quality parameters here
+   layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Freq", "Peak Freq", juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f), 750.f)); // Added the peak frequency parameter inside the value tree
+   layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Gain", "Peak Gain", juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f), 0.f)); // Added the peak gain parameter inside the value tree
+   layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Quality", "Peak Quality", juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 1.f), 1.f));
+
+   juce::StringArray stringArray;
+   for (int i = 0; i < 4; i++)
+   {
+       juce::String str;
+       str << (12 + i*12);
+       str << " db/Oct";
+       stringArray.add(str);
+   }
+
+   layout.add(std::make_unique<juce::AudioParameterChoice>("LowCut Slope", "LowCut Slope", stringArray, 0)); // Added the lowCut slope choices to the parameter tree
+   layout.add(std::make_unique<juce::AudioParameterChoice>("HighCut Slope", "HighCut Slope", stringArray, 0)); // Added the highcut slope choices to the parameter tree
+
+   return layout; // Returns the layout to our apvts variable
 }
 
 //==============================================================================
